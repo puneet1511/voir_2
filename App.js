ddd30e1f8eb5a98e6e3180510b2e605eb077fc61
puneet1511 <35158460+puneet1511@users.noreply.github.com>
2022-01-29 21:28:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/image_hkd.jpeg'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+       <Text style={styles.instructions2}>
+      Voir - The true social media</Text>
+      <Image source={logo} style={styles.logo} /> 
+
+      <Text style={styles.instructions}>
+      To share reviews with your friends, just press the button below!</Text>
       <StatusBar style="auto" />
+
+       <TouchableOpacity
+        onPress={() => alert('Hello, welcome to your sharing space!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Share a Review</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -18,4 +32,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 10,
+    marginBottom: 20,
+  },
+  instructions2: {
+    color: '#fff',
+    fontSize: 25,
+    marginHorizontal: 10,
+    marginBottom: 20,
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: "green",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },  
 });
